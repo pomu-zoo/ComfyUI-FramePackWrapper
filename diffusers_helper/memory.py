314ceddfe5cc1e4f5429cd485d4a909edbd36f70
pomu_zoo <pomuhei@me.com>
2025-05-02 15:18:49 +0900

    recommint

--- diffusers_helper/memory.py
+++ diffusers_helper/memory.py
@@ -72,14 +72,15 @@ def get_xpu_free_memory_gb(device=None):
     if device is None:
         device = gpu
 
-    memory_stats = torch.xpu.memory_stats(device)
-    bytes_active = memory_stats['active_bytes.all.current']
-    bytes_reserved = memory_stats['reserved_bytes.all.current']
-    bytes_free_xpu, _ = torch.xpu.mem_get_info(device)
-    bytes_inactive_reserved = bytes_reserved - bytes_active
-    bytes_total_available = bytes_free_xpu + bytes_inactive_reserved
-    return bytes_total_available / (1024 ** 3)
-
+    if device.type == "xpu":
+        try:
+            props = torch.xpu.get_device_properties(device)
+            total_memory = props.total_memory
+            allocated = torch.xpu.memory_allocated(device)
+            free = total_memory - allocated
+            return free / (1024 ** 3)
+        except Exception as e:
+            print("[Warning] Failed to get XPU memory info:", e)
 
 def move_model_to_device_with_memory_preservation(model, target_device, preserved_memory_gb=0):
     print(f'Moving {model.__class__.__name__} to {target_device} with preserved memory: {preserved_memory_gb} GB')
